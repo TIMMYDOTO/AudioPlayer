6cc90fce77b517f8e4ffcab2010f9ca8a4adaf19
Artyom Schiopu <aartyom3@gmail.com>
2017-07-07 15:54:48 +0300

    added recording audio

---
+++ Audio/ViewController3.swift
@@ -0,0 +1,264 @@
+//
+//  ViewController3.swift
+//  Audio
+//
+//  Created by User1 on 7/7/17.
+//  Copyright © 2017 User1. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class ViewController3: UIViewController {
+    var soundFileURL:URL!
+
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBAction func record(_ sender: Any) {
+        if player != nil && player.isPlaying {
+            print("stopping")
+            player.stop()
+        }
+        
+        if recorder == nil {
+            print("recording. recorder nil")
+            recordButton.setTitle("Pause", for:.normal)
+            playButton.isEnabled = false
+            stopButton.isEnabled = true
+            recordWithPermission(true)
+            return
+        }
+        
+        if recorder != nil && recorder.isRecording {
+            print("pausing")
+            recorder.pause()
+            recordButton.setTitle("Continue", for:.normal)
+            
+        } else {
+            print("recording")
+            recordButton.setTitle("Pause", for:.normal)
+            playButton.isEnabled = false
+            stopButton.isEnabled = true
+            //            recorder.record()
+           recordWithPermission(false)
+        }
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        recorder?.stop()
+        player?.stop()
+        
+     
+        
+        recordButton.setTitle("Record", for: .normal)
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setActive(false)
+            playButton.isEnabled = true
+            stopButton.isEnabled = false
+            recordButton.isEnabled = true
+        } catch {
+            print("could not make session inactive")
+            print(error.localizedDescription)
+        }
+    }
+    @IBAction func play(_ sender: Any) {
+         play()
+    }
+    
+
+    func play() {
+        print("\(#function)")
+        
+        
+        var url:URL?
+        if self.recorder != nil {
+            url = self.recorder.url
+        } else {
+            url = self.soundFileURL!
+        }
+        print("playing \(String(describing: url))")
+        
+        do {
+            self.player = try AVAudioPlayer(contentsOf: url!)
+            stopButton.isEnabled = true
+            player.delegate = self
+            player.prepareToPlay()
+            player.volume = 1.0
+            player.play()
+        } catch {
+            self.player = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    
+    
+    
+    var recorder: AVAudioRecorder!
+    
+    var player:AVAudioPlayer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("\(#function)")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        recorder = nil
+        player = nil
+        // Dispose of any resources that can be recreated.
+    }
+    func setupRecorder() {
+       
+        
+        let format = DateFormatter()
+        format.dateFormat="yyyy-MM-dd-HH-mm-ss"
+        let currentFileName = "recording-\(format.string(from: Date())).m4a"
+        print(currentFileName)
+        
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        self.soundFileURL = documentsDirectory.appendingPathComponent(currentFileName)
+        print("writing to soundfile url: '\(soundFileURL!)'")
+        
+        if FileManager.default.fileExists(atPath: soundFileURL.absoluteString) {
+            // probably won't happen. want to do something about it?
+            print("soundfile \(soundFileURL.absoluteString) exists")
+        }
+        
+        let recordSettings:[String : Any] = [
+            AVFormatIDKey:             kAudioFormatAppleLossless,
+            AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
+            AVEncoderBitRateKey :      32000,
+            AVNumberOfChannelsKey:     2,
+            AVSampleRateKey :          44100.0
+        ]
+        
+        
+        do {
+            recorder = try AVAudioRecorder(url: soundFileURL, settings: recordSettings)
+            recorder.delegate = self
+            recorder.isMeteringEnabled = true
+            recorder.prepareToRecord() // creates/overwrites the file at soundFileURL
+        } catch {
+            recorder = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    func recordWithPermission(_ setup:Bool) {
+        print("\(#function)")
+        
+        AVAudioSession.sharedInstance().requestRecordPermission() {
+            [unowned self] granted in
+            if granted {
+                
+                DispatchQueue.main.async {
+                    print("Permission to record granted")
+                    self.setSessionPlayAndRecord()
+                    if setup {
+                        self.setupRecorder()
+                    }
+                    self.recorder.record()
+                    
+                  
+                }
+            } else {
+                print("Permission to record not granted")
+            }
+        }
+        
+        if AVAudioSession.sharedInstance().recordPermission() == .denied {
+            print("permission denied")
+        }
+    }
+    func setSessionPlayAndRecord() {
+        print("\(#function)")
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+        } catch {
+            print("could not set session category")
+            print(error.localizedDescription)
+        }
+        
+        do {
+            try session.setActive(true)
+        } catch {
+            print("could not make session active")
+            print(error.localizedDescription)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension ViewController3 : AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder,
+                                         successfully flag: Bool) {
+        
+        print("\(#function)")
+        
+        print("finished recording \(flag)")
+        stopButton.isEnabled = false
+        playButton.isEnabled = true
+        recordButton.setTitle("Record", for:UIControlState())
+        
+        // iOS8 and later
+        let alert = UIAlertController(title: "Recorder",
+                                      message: "Finished Recording",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Keep", style: .default, handler: {action in
+            print("keep was tapped")
+            self.recorder = nil
+        }))
+        alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: {action in
+            print("delete was tapped")
+            self.recorder.deleteRecording()
+        }))
+        self.present(alert, animated:true, completion:nil)
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder,
+                                          error: Error?) {
+        print("\(#function)")
+        
+        if let e = error {
+            print("\(e.localizedDescription)")
+        }
+    }
+    
+}
+
+// MARK: AVAudioPlayerDelegate
+extension ViewController3 : AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("\(#function)")
+        
+        print("finished playing \(flag)")
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("\(#function)")
+        
+        if let e = error {
+            print("\(e.localizedDescription)")
+        }
+        
+    }
+}
+
